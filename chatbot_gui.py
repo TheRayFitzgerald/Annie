3af3bc8d2752243a0f3fe25f8f83c678ddc0636d
Ray Fitzgerald <117381503@umail.ucc.ie>
2020-03-15 22:39:38 +0000

    removed redundant code from gui

--- chatbot_gui.py
+++ chatbot_gui.py
@@ -19,38 +19,6 @@ pd.options.display.max_rows = 4000
 pd.options.display.max_seq_items = 2000
 
 
-
-def setup():
-    base = Tk()
-    base.title("Hello")
-    base.geometry("400x500")
-    base.resizable(width=FALSE, height=FALSE)
-
-    #Create Chat window
-    ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
-
-    ChatLog.config(state=DISABLED)
-
-    #Bind scrollbar to Chat window
-    scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
-    ChatLog['yscrollcommand'] = scrollbar.set
-
-    #Create Button to send message
-    SendButton = Button(base, font=("Verdana",12,'bold'), text="Send", width="12", height=5,
-                        bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff',
-                        command= send )
-
-    #Create the box to enter message
-    EntryBox = Text(base, bd=0, bg="white",width="29", height="5", font="Arial")
-    #EntryBox.bind("<Return>", send)
-
-
-    #Place all components on the screen
-    scrollbar.place(x=376,y=6, height=386)
-    ChatLog.place(x=6,y=6, height=386, width=370)
-    EntryBox.place(x=128, y=401, height=90, width=265)
-    SendButton.place(x=6, y=401, height=90)
-
 def main():
     df=pd.read_csv('./corpora/conglomerate.csv')
     df['lemmatized_text']=df['context'].apply(data_clean.text_normalization)
