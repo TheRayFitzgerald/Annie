00d479484cd935fc483aaf121c1ebe8be0e1ec95
Ray Fitzgerald <117381503@umail.ucc.ie>
2020-03-15 17:01:56 +0000

    changes to how chatbot is ran so that the corpus is not reloadede w/ every messsage

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #! usr/bin/python3
-
 import pandas as pd
 import nltk
 import numpy as np
@@ -13,21 +12,27 @@ from nltk import word_tokenize # to create tokens
 from nltk.corpus import stopwords # for stop words
 import bow
 from cosine_similarity import cosine_similarity
+import chatbot_gui
 pd.options.display.max_rows = 4000
 pd.options.display.max_seq_items = 2000
 
-def main(QUESTION):
-    df=pd.read_csv('./corpora/corpus_anxiety.csv')
+
+def main():
+    df=pd.read_csv('./corpora/conglomerate.csv')
     df['lemmatized_text']=df['context'].apply(data_clean.text_normalization)
     df_bow, cv=bow.context_bow(df)
-    question_bow=bow.user_question_bow(QUESTION, cv)
-    # create new column for cosine similarity
-    df['similarity_bow']=cosine_similarity(df_bow, question_bow)
+    chatbot_gui.main()
+    message = chatbot_gui.send()
+    while question != 'exit':
+        question_bow=bow.user_question_bow(QUESTION, cv)
+        # create new column for cosine similarity
+        df['similarity_bow']=cosine_similarity(df_bow, question_bow)
 
-    # taking similarity value of responses for the reddit context questions
-    df_simi = pd.DataFrame(df, columns=['text response','similarity_bow'])
-    # sort the values to get answer to *most similar question*
-    df_simi_sort = df_simi.sort_values(by='similarity_bow', ascending=False)
-    return df_simi_sort.iloc[0]['text response']
+        # taking similarity value of responses for the reddit context questions
+        df_simi = pd.DataFrame(df, columns=['text response','similarity_bow'])
+        # sort the values to get answer to *most similar question*
+        df_simi_sort = df_simi.sort_values(by='similarity_bow', ascending=False)
+        chatbot_gui.recieve(df_simi_sort.iloc[0]['text response'])
+
 if __name__ == '__main__':
     main()
